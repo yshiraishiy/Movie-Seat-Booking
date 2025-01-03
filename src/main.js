b22a12f3a46f4101da6993c922644e1bbab7356b
yshiraishiy <yuya0505soccer@gmail.com>
2025-01-03 13:13:04 +0900

    ✨データをローカルストレージに保存

--- src/main.js
+++ src/main.js
@@ -6,9 +6,22 @@ const movieSelect = document.getElementById("movie");
 
 let ticketPrice = +movieSelect.value;
 
+// 選択した映画のインデックスと価格を保存
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
 // 指定した座席数と合計金額を更新
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  // 選択した座席を配列にコピー、配列をマップし、新しいインデックスの配列を返す
+  const seatsIndex = [...selectedSeats].map(function (seat) {
+    return [...seats].indexOf(seat);
+  });
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
 
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
@@ -18,6 +31,7 @@ function updateSelectedCount() {
 // 映画を選択時のイベント
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
